272f7ab240e373a1908b6fb9309fabd50d1ddf9a
naeFAe <82336045+naeFAe@users.noreply.github.com>
2021-10-31 01:20:52 +0300

    Add files via upload

---
+++ proiect/README.md
@@ -0,0 +1,2 @@
+# proiect-car_rental
+ 
---
+++ proiect/cmake-build-debug/CMakeFiles/proiect.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proiect.dir/main.cpp.obj"
+  "libproiect.dll.a"
+  "proiect.exe"
+  "proiect.exe.manifest"
+  "proiect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proiect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
